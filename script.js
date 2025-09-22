08db07c790354e351f4a35c71fb23076b4afb141
Ajayvarma-github <ajayvarmagadiraju58@gmail.com>
2025-09-22 15:21:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function easy() {
-    document.getElementById('circle').style.animationDuration = '17s';
-    document.getElementById('circle').className = 'crcl';
-}
-function medium() {
-    document.getElementById('circle').style.animationDuration = '14s';
-    document.getElementById('circle').className = 'crcl';
-}
-function hard() {
-    document.getElementById('circle').style.animationDuration = '10s';
-    document.getElementById('circle').className = 'crcl';
-}
-let cnt = 0;
-function count() {
-    cnt = parseInt(1) + parseInt(cnt);
-    var scr = document.getElementById('score');
-    scr.innerHTML = cnt;
-}
-function restart() {
-    window.location.reload();
-}
+<script>
+    let cnt = 0;
+    function easy() {
+        document.getElementById('circle').style.animationDuration = '17s';
+        document.getElementById('circle').className = 'crcl';
+    }
+    function medium() {
+        document.getElementById('circle').style.animationDuration = '14s';
+        document.getElementById('circle').className = 'crcl';
+    }
+    function hard() {
+        document.getElementById('circle').style.animationDuration = '10s';
+        document.getElementById('circle').className = 'crcl';
+    }
+    function count() {
+        cnt += 1;
+        document.getElementById('score').innerHTML = cnt;
+    }
+    function restart() {
+        window.location.reload();
+    }
+  </script>
