c1ae2db7e97395584d28d11dd93a7f399b4ccfa1
Ajayvarma-github <ajayvarmagadiraju58@gmail.com>
2025-09-22 15:41:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let cnt = 0;
 function easy() {
     const circle = document.getElementById('circle');
     circle.className = 'crcl';
-    circle.style.animation = 'movement 17s linear infinite';
+    circle.style.animation = 'movement 17s linear forwards';
 }
 function medium() {
     const circle = document.getElementById('circle');
     circle.className = 'crcl';
-    circle.style.animation = 'movement 14s linear infinite';
+    circle.style.animation = 'movement 14s linear forwards';
 }
 function hard() {
     const circle = document.getElementById('circle');
     circle.className = 'crcl';
-    circle.style.animation = 'movement 10s linear infinite';
+    circle.style.animation = 'movement 10s linear forwards';
 }
 function count() {
     cnt += 1;
@@ -21,4 +21,5 @@ function count() {
 function restart() {
     window.location.reload();
 }
+
 
