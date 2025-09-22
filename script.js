776cd2b9769ed4de77fd4f297a46aed8558fdc0f
Ajayvarma-github <ajayvarmagadiraju58@gmail.com>
2025-09-22 15:36:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+let cnt = 0;
 function easy() {
-    document.getElementById('circle').style.animationDuration = '17s';
-    document.getElementById('circle').className = 'crcl';
+    const circle = document.getElementById('circle');
+    circle.className = 'crcl';
+    circle.style.animation = 'movement 17s linear infinite';
 }
 function medium() {
-    document.getElementById('circle').style.animationDuration = '14s';
-    document.getElementById('circle').className = 'crcl';
+    const circle = document.getElementById('circle');
+    circle.className = 'crcl';
+    circle.style.animation = 'movement 14s linear infinite';
 }
 function hard() {
-    document.getElementById('circle').style.animationDuration = '10s';
-    document.getElementById('circle').className = 'crcl';
+    const circle = document.getElementById('circle');
+    circle.className = 'crcl';
+    circle.style.animation = 'movement 10s linear infinite';
 }
-let cnt = 0;
 function count() {
-    cnt = parseInt(1) + parseInt(cnt);
-    var scr = document.getElementById('score');
-    scr.innerHTML = cnt;
+    cnt += 1;
+    document.getElementById('score').innerText = cnt;
 }
 function restart() {
     window.location.reload();
 }
+
